5d11565fc07443b82242c384c17f5f594a83b7f5
nawaphonST1 <nawaphonsungthong@email.com>
2025-03-08 01:32:27 +0700

    ignore eslint

--- a/next.config.ts
+++ a/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    ignoreDuringBuilds: true, // ปิด ESLint ระหว่าง build
+  },
 };
 
 export default nextConfig;
